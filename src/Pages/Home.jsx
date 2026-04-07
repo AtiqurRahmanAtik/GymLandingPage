cfad52db5c0ebeca62c00b3335daf76b579a1caf
AtiqurRahmanAtik <atikahmedtanvir84@gmail.com>
2026-04-08 00:14:03 +0600

    complete product section with responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import NewsletterSubscribe from '../Components/NewsletterSubscribe/NewsletterSubscribe';
 import Testimonials from '../Components/Testimonials/Testimonials';
 import TrainingSection from '../Components/TrainingSection/TrainingSection';
-
+import ProductsSection from '../Components/ProductsSection/ProductsSection';
 
 const Home = () => {
     return (
         <>
             <h1 className='text-2xl text-center my-7 text-red-500'>Home page </h1>
 
-           
+            <ProductsSection/>
 
             <TrainingSection/>
 
